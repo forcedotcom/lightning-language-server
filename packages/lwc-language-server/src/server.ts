5253fd9b97c458996bd214feadf67d5f58410016
Pierre-Marie <p.dartus@salesforce.com>
2017-06-07 11:40:10 -0700

    Remove unused imports

--- packages/lwc-language-server/src/server.ts
+++ packages/lwc-language-server/src/server.ts
@@ -8,8 +8,6 @@ import {
     InitializeResult,
     TextDocumentPositionParams,
     CompletionItem,
-    SymbolInformation,
-    DocumentSymbolParams,
     Files,
 } from 'vscode-languageserver';
 
