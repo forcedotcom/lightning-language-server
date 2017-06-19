e74247aad6fa12bd1e431b2602d1535a4e684d15
Pierre-Marie <p.dartus@salesforce.com>
2017-06-19 10:25:07 -0700

    Add simple auto-complete support in the template

--- packages/lwc-language-server/src/template/__test__/completion.test.ts
+++ packages/lwc-language-server/src/template/__test__/completion.test.ts
@@ -1,30 +1,72 @@
-import { TextDocument, Position } from 'vscode-languageserver';
+import {
+    TextDocument,
+    Position,
+    CompletionItem,
+    TextEdit,
+} from 'vscode-languageserver';
 import templateCompletion from '../completion';
 
-function createAutocomplite(content: string) {
-    const [before, after] = content.split('|');
-
-    return {
-        document: TextDocument.create(
-            'test://test.html',
-            'html',
-            0,
-            before + after,
-        ),
-        position: Position.create(0, before.length),
-    };
+interface ICompletionMatcher {
+    label: string;
+    result: string;
 }
 
-it('in a tag body', () => {
-    const { document, position } = createAutocomplite(
-        '<template> | </template>',
-    );
-    const items = templateCompletion(document, position);
-    expect(items).toHaveLength(0);
-});
+function applyEdit(document: TextDocument, edit: TextEdit): string {
+    let text = document.getText();
+    const startOffset = document.offsetAt(edit.range.start);
+    const endOffset = document.offsetAt(edit.range.end);
+    text =
+        text.substring(0, startOffset) +
+        edit.newText +
+        text.substring(endOffset, text.length);
+    return text;
+}
 
-it('in a element start tag', () => {
-    const { document, position } = createAutocomplite('<template><div | ');
+function testCompletion(content: string, matchers: ICompletionMatcher[] = []) {
+    const [before, after] = content.split('|');
+    const document = TextDocument.create(
+        'test://test.html',
+        'html',
+        0,
+        before + after,
+    );
+    const position = Position.create(0, before.length);
     const items = templateCompletion(document, position);
-    expect(items).toHaveLength(5);
+
+    matchers.forEach(matcher => {
+        const item = items.find(candidate => matcher.label === candidate.label);
+        expect(item).toBeDefined();
+        expect(applyEdit(document, item.textEdit)).toEqual(matcher.result);
+    });
+
+    return items;
+}
+
+let res: CompletionItem[];
+
+it('complete', () => {
+    res = testCompletion('<template>|</template>');
+    expect(res).toHaveLength(0);
+
+    res = testCompletion('<template |');
+    expect(res).toHaveLength(0);
+
+    testCompletion('<template><div |', [
+        { label: 'if:true', result: '<template><div if:true={$1}' },
+        { label: 'for:item', result: '<template><div for:item="$1"' },
+    ]);
+
+    testCompletion('<template><div if|', [
+        { label: 'if:true', result: '<template><div if:true={$1}' },
+        { label: 'if:false', result: '<template><div if:false={$1}' },
+    ]);
+
+    testCompletion('<template><div i|f', [
+        { label: 'if:true', result: '<template><div if:true={$1}' },
+        { label: 'if:false', result: '<template><div if:false={$1}' },
+    ]);
+
+    testCompletion('<template><div if:|true={isTrue}', [
+        { label: 'if:true', result: '<template><div if:true={isTrue}' },
+    ]);
 });
