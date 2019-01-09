c1e72c482c72199ab5948c958292b6d2d8c2ad15
Nicolas Kruk <nkruk@salesforce.com>
2019-01-09 10:47:23 -0500

    move aura_types

--- packages/aura-language-server/copy-static-assets.ts
+++ packages/aura-language-server/copy-static-assets.ts
@@ -1,6 +1,7 @@
 import * as shell from "shelljs";
 
 shell.cp("-R", "src/*.json", "lib/");
+shell.cp("-R", "src/tern-server/*.json", "lib/tern-server/");
 // Copy Html Language Service files
 shell.cp("-R", "src/html-language-service/beautify/*.js", "lib/html-language-service/beautify/");
 shell.mkdir("-p", "lib/html-language-service/beautify/esm/");
