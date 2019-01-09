85e696f9ca7f70320a0857d9b6eba92592ef4581
Nicolas Kruk <nkruk@salesforce.com>
2019-01-09 11:22:31 -0500

    stubs

--- packages/aura-language-server/copy-static-assets.ts
+++ packages/aura-language-server/copy-static-assets.ts
@@ -1,7 +1,7 @@
 import * as shell from "shelljs";
 
-shell.cp("-R", "src/*.json", "lib/");
 shell.cp("-R", "src/tern-server/*.json", "lib/tern-server/");
+shell.cp("-R", "src/markup/*.json", "lib/markup/");
 // Copy Html Language Service files
 shell.cp("-R", "src/html-language-service/beautify/*.js", "lib/html-language-service/beautify/");
 shell.mkdir("-p", "lib/html-language-service/beautify/esm/");
