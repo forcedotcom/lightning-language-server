cc94b2700066a866b73837131adac64f4d6ccd59
Min Idzelis <midzelis@salesforce.com>
2019-01-15 17:48:11 -0500

    Copy from lwc-language-server

---
+++ packages/lightning-lsp-common/src/__test__/config.test.ts
@@ -0,0 +1,41 @@
+import * as utils from '../utils';
+import * as config from '../config';
+import * as path from 'path';
+import { WorkspaceContext } from '../context';
+import { FORCE_APP_ROOT, UTILS_ROOT } from './test-utils';
+
+it('lifecycle', async () => {
+    const context = new WorkspaceContext('test-workspaces/sfdx-workspace');
+    await context.configureAndIndex();
+
+    // verify jsconfig.json after indexing
+    const jsconfigPathForceApp = FORCE_APP_ROOT + '/lwc/jsconfig.json';
+    let jsconfigForceApp: config.IJsconfig = JSON.parse(utils.readFileSync(jsconfigPathForceApp));
+    expect(jsconfigForceApp.compilerOptions.baseUrl).toBe('.');
+    expect(jsconfigForceApp.compilerOptions.paths).toMatchObject({
+        'c/hello_world': ['hello_world/hello_world.js'],
+        'c/todo_utils': ['../../../../utils/meta/lwc/todo_utils/todo_utils.js'],
+    });
+    const jsconfigPathUtils = UTILS_ROOT + '/lwc/jsconfig.json';
+    const jsconfigUtils = JSON.parse(utils.readFileSync(jsconfigPathUtils));
+    expect(jsconfigUtils.compilerOptions.baseUrl).toBe('.');
+    expect(jsconfigUtils.compilerOptions.paths).toMatchObject({
+        'c/hello_world': ['../../../force-app/main/default/lwc/hello_world/hello_world.js'],
+        'c/todo_utils': ['todo_utils/todo_utils.js'],
+    });
+
+    // onCreateCustomComponent:
+    const newCompTag = 'c/new_comp';
+    const newCompPath = UTILS_ROOT + path.join('meta', 'lwc', 'new_comp', 'new_comp.js');
+    expect(jsconfigForceApp.compilerOptions.paths[newCompTag]).toBeUndefined();
+    config.onCreatedCustomComponent(context, newCompPath);
+    jsconfigForceApp = JSON.parse(utils.readFileSync(jsconfigPathForceApp));
+    expect(jsconfigForceApp.compilerOptions.paths[newCompTag]).toEqual(['../../../../utils/metameta/lwc/new_comp/new_comp.js']);
+
+    // onDeleteCustomComponent:
+    config.onDeletedCustomComponent(context, newCompPath);
+    jsconfigForceApp = JSON.parse(utils.readFileSync(jsconfigPathForceApp));
+    expect(jsconfigForceApp.compilerOptions.paths[newCompTag]).toBeUndefined();
+    // no error deleting non-existing:
+    config.onDeletedCustomComponent(context, newCompPath);
+});
---
+++ packages/lightning-lsp-common/src/config.ts
@@ -0,0 +1,96 @@
+import { WorkspaceContext } from './context';
+import { moduleFromFile } from './metadata-utils/custom-components-util';
+import { WorkspaceType } from './shared';
+import * as utils from './utils';
+import * as path from 'path';
+
+// file ==> add to jsconfig.json "paths" {tag, [relativePath]}, see http://www.typescriptlang.org/docs/handbook/module-resolution.html
+
+export interface IPaths {
+    [tag: string]: string[];
+}
+export interface IJsconfig {
+    compilerOptions: {
+        baseUrl?: string;
+        paths?: IPaths;
+    };
+}
+
+export function onIndexCustomComponents(context: WorkspaceContext, files: string[]) {
+    // set paths for all current components in all the projects jsconfig.json files
+    context.getRelativeModulesDirs().forEach(relativeModulesDir => {
+        const modulesDir = path.join(context.workspaceRoot, relativeModulesDir);
+
+        const paths: IPaths = {};
+        for (const file of files) {
+            const tag = moduleFromFile(file, context.type === WorkspaceType.SFDX);
+            // path must be relative to location of jsconfig.json
+            const relativeFilePath = utils.relativePath(modulesDir, file);
+            paths[tag] = [relativeFilePath];
+        }
+
+        // set "paths" in jsconfig.json
+        const relativeJsConfigPath = path.join(relativeModulesDir, 'jsconfig.json');
+        const jsconfigFile = path.join(context.workspaceRoot, relativeJsConfigPath);
+        const jsconfig: IJsconfig = JSON.parse(utils.readFileSync(jsconfigFile));
+        if (
+            !jsconfig.compilerOptions ||
+            !jsconfig.compilerOptions.hasOwnProperty('baseUrl') ||
+            jsconfig.compilerOptions.baseUrl !== '.' ||
+            !jsconfig.compilerOptions.hasOwnProperty('paths') ||
+            JSON.stringify(jsconfig.compilerOptions.paths) !== JSON.stringify(paths)
+        ) {
+            if (!jsconfig.compilerOptions) {
+                jsconfig.compilerOptions = {};
+            }
+            jsconfig.compilerOptions.baseUrl = '.';
+            jsconfig.compilerOptions.paths = paths;
+            writeJsconfig(jsconfigFile, jsconfig);
+        }
+    });
+}
+
+export function onCreatedCustomComponent(context: WorkspaceContext, file: string) {
+    // add tag/path to component to all the project's jsconfig.json "paths"
+    const tag = moduleFromFile(file, context.type === WorkspaceType.SFDX);
+    context.getRelativeModulesDirs().forEach(relativeModulesDir => {
+        const modulesDir = path.join(context.workspaceRoot, relativeModulesDir);
+
+        // path must be relative to location of jsconfig.json
+        const relativeFilePath = utils.relativePath(modulesDir, file);
+
+        // update "paths" in jsconfig.json
+        const relativeJsConfigPath = path.join(relativeModulesDir, 'jsconfig.json');
+        const jsconfigFile = path.join(context.workspaceRoot, relativeJsConfigPath);
+        const jsconfig: IJsconfig = JSON.parse(utils.readFileSync(jsconfigFile));
+        if (!jsconfig.compilerOptions) {
+            jsconfig.compilerOptions = {};
+        }
+        jsconfig.compilerOptions.baseUrl = '.';
+        if (!jsconfig.compilerOptions.paths) {
+            jsconfig.compilerOptions.paths = {};
+        }
+        jsconfig.compilerOptions.paths[tag] = [relativeFilePath];
+        writeJsconfig(jsconfigFile, jsconfig);
+    });
+}
+
+export function onDeletedCustomComponent(context: WorkspaceContext, file: string) {
+    // delete tag from all the project's jsconfig.json "paths"
+    const tag = moduleFromFile(file, context.type === WorkspaceType.SFDX);
+    context.getRelativeModulesDirs().forEach(relativeModulesDir => {
+        const relativeJsConfigPath = path.join(relativeModulesDir, 'jsconfig.json');
+        const jsconfigFile = path.join(context.workspaceRoot, relativeJsConfigPath);
+        const jsconfig: IJsconfig = JSON.parse(utils.readFileSync(jsconfigFile));
+        if (jsconfig.compilerOptions) {
+            if (jsconfig.compilerOptions.paths) {
+                delete jsconfig.compilerOptions.paths[tag];
+                writeJsconfig(jsconfigFile, jsconfig);
+            }
+        }
+    });
+}
+
+export function writeJsconfig(file: string, jsconfig: {}) {
+    utils.writeFileSync(file, JSON.stringify(jsconfig, null, 4));
+}
