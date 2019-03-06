e2744b7f2d3bef589ed144509ed8c8f12faa26ed
Min Idzelis <midzelis@salesforce.com>
2019-03-06 12:17:58 -0500

    Normalize paths in tests (#6)

--- packages/aura-language-server/src/aura-indexer/__tests__/indexer.spec.ts
+++ packages/aura-language-server/src/aura-indexer/__tests__/indexer.spec.ts
@@ -1,17 +1,41 @@
 import { utils, WorkspaceContext } from 'lightning-lsp-common';
 import AuraIndexer from '../indexer';
+import * as path from 'path';
+import URI from 'vscode-uri';
+
+function normalize(start: string, p: string) {
+    if (p.startsWith(start)) {
+        return p.slice(start.length + 1);
+    }
+    return p;
+}
+function uriToFile(uri: string): string {
+    return URI.parse(uri).fsPath;
+}
 
 it('aura indexer', async () => {
-    const context = new WorkspaceContext('test-workspaces/sfdx-workspace');
+    const ws = 'test-workspaces/sfdx-workspace';
+    const full = path.resolve(ws);
+
+    const context = new WorkspaceContext(ws);
     await context.configureProject();
     const auraIndexer = new AuraIndexer(context);
     await auraIndexer.configureAndIndex();
     context.addIndexingProvider({ name: 'aura', indexer: auraIndexer });
 
-    const markup = await context.findAllAuraMarkup();
+    let markup = await context.findAllAuraMarkup();
+    markup = markup.map(p => normalize(full, p));
     expect(markup).toMatchSnapshot();
 
     const tags = auraIndexer.getAuraTags();
+    tags.forEach(taginfo => {
+        if (taginfo.file) {
+            taginfo.file = normalize(full, taginfo.file);
+        }
+        if (taginfo.location && taginfo.location.uri) {
+            taginfo.location.uri = normalize(full, uriToFile(taginfo.location.uri));
+        }
+    });
     expect(tags).toMatchSnapshot();
 
     const namespaces = auraIndexer.getAuraNamespaces();
