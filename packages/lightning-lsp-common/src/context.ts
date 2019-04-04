3ecd804f1abbddaadfa2e311d936c6d0a00bfbc6
Nicolas Kruk <nkruk@salesforce.com>
2019-04-03 21:17:09 -0400

    settings.json file getting overwritten unintentionally https://github.com/forcedotcom/salesforcedx-vscode/issues/1210

--- packages/lightning-lsp-common/src/context.ts
+++ packages/lightning-lsp-common/src/context.ts
@@ -249,10 +249,10 @@ export class WorkspaceContext {
                     const jsConfigPath = join(this.workspaceRoot, relativeJsConfigPath);
                     const relativeWorkspaceRoot = utils.relativePath(path.dirname(jsConfigPath), this.workspaceRoot);
                     jsConfigContent = this.processTemplate(jsConfigTemplate, { project_root: relativeWorkspaceRoot });
-                    await this.updateConfigFile(relativeJsConfigPath, jsConfigContent);
+                    this.updateConfigFile(relativeJsConfigPath, jsConfigContent);
                     // write/update .eslintrc.json
                     const relativeEslintrcPath = join(relativeModulesDir, '.eslintrc.json');
-                    await this.updateConfigFile(relativeEslintrcPath, eslintrcTemplate);
+                    this.updateConfigFile(relativeEslintrcPath, eslintrcTemplate);
                     await this.updateForceIgnoreFile(forceignore);
                 }
                 break;
@@ -262,7 +262,7 @@ export class WorkspaceContext {
                 jsConfigContent = this.processTemplate(jsConfigTemplate, { project_root: '../..' });
                 for (const relativeModulesDir of relativeModulesDirs) {
                     const relativeJsConfigPath = join(relativeModulesDir, 'jsconfig.json');
-                    await this.updateConfigFile(relativeJsConfigPath, jsConfigContent);
+                    this.updateConfigFile(relativeJsConfigPath, jsConfigContent);
                 }
                 break;
 
@@ -271,7 +271,7 @@ export class WorkspaceContext {
                 jsConfigContent = this.processTemplate(jsConfigTemplate, { project_root: '../..' });
                 for (const relativeModulesDir of relativeModulesDirs) {
                     const relativeJsConfigPath = join(relativeModulesDir, 'jsconfig.json');
-                    await this.updateConfigFile(relativeJsConfigPath, jsConfigContent);
+                    this.updateConfigFile(relativeJsConfigPath, jsConfigContent);
                 }
                 break;
         }
@@ -383,7 +383,7 @@ export class WorkspaceContext {
         const configFile = join(this.workspaceRoot, relativeConfigPath);
         try {
             const configJson = JSON.parse(config);
-            if (!fs.pathExists(configFile)) {
+            if (!fs.pathExistsSync(configFile)) {
                 utils.writeJsonSync(configFile, configJson);
             } else {
                 try {
