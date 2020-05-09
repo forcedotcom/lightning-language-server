1f569e23fc7fe5551c722d6aa268472e6483e177
muenzpraeger <mail@winkelmeyer.com>
2020-05-09 16:11:07 +0200

    fix: remove OSS test components

--- test-workspaces/oss-lwc-workspace/src/main.js
+++
@@ -1,6 +0,0 @@
-import {createElement} from "lwc";
-import App from "my-app";
-
-const container = document.getElementById("main");
-const element = createElement("my-app", {is: App});
-container.appendChild(element);
