2e854470387c6e3b54bcc5f335c2f205d8162efb
Micah Cooper <micah+github@mrmicahcooper.com>
2020-05-29 15:59:34 -0400

    Generate static resource typing

--- packages/lwc-language-server/src/__tests__/typing.test.ts
+++ packages/lwc-language-server/src/__tests__/typing.test.ts
@@ -2,15 +2,24 @@ import Typing from '../typing';
 
 describe('Typing', () => {
     it('generates the typing declaration for a content asset file.', () => {
-        const contentAssetMetaFileName: string = 'logo.asset-meta.xml';
-
+        const contentAssetMetaFilename: string = 'logo.asset-meta.xml';
         const expectedDeclaration: string = `declare module "@salesforce/contentAssetUrl/logo" {
     var logo: string;
     export default logo;
 }
 `;
 
-        const declaration = Typing.declaration(contentAssetMetaFileName);
-        expect(declaration).toEqual(expectedDeclaration);
+        expect(Typing.declaration(contentAssetMetaFilename)).toEqual(expectedDeclaration);
+    });
+
+    it('generate the typing declaration for a static resource file', () => {
+        const staticResourceMetaFilename: string = 'd3.resource-meta.xml';
+        const expectedDeclaration: string = `declare module "@salesforce/resourceUrl/d3" {
+    var d3: string;
+    export default d3;
+}
+`;
+
+        expect(Typing.declaration(staticResourceMetaFilename)).toEqual(expectedDeclaration);
     });
 });
