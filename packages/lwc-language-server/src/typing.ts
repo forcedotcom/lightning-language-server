968208ed74099ae7e8f388bad4e66cf880b32ee5
Micah Cooper <micah+github@mrmicahcooper.com>
2020-05-29 16:23:44 -0400

    Generate message channel declaration

--- packages/lwc-language-server/src/typing.ts
+++ packages/lwc-language-server/src/typing.ts
@@ -2,18 +2,23 @@ export default class Typing {
     public static declaration(metaFilename: string): string {
         const regex = /^(?<name>.+)\.(?<metaType>.+)-meta.xml$/;
         const { name, metaType } = regex.exec(metaFilename).groups;
-        const typeMap: { [key: string]: string } = {
-            asset: 'contentAssetUrl',
-            resource: 'resourceUrl',
-        };
+        let modulePath: string;
 
-        const type: string = typeMap[metaType];
-
-        if (type === undefined) {
-            throw new Error(`meta file ${metaType} not supported`);
+        switch (metaType) {
+            case 'asset':
+                modulePath = `@salesforce/contentAssetUrl/${name}`;
+                break;
+            case 'resource':
+                modulePath = `@salesforce/resourceUrl/${name}`;
+                break;
+            case 'messageChannel':
+                modulePath = `@salesforce/messageChannel/${name}__c`;
+                break;
+            default:
+                throw new Error(`meta file ${metaType} not supported`);
         }
 
-        return `declare module "@salesforce/${type}/${name}" {
+        return `declare module "${modulePath}" {
     var ${name}: string;
     export default ${name};
 }
