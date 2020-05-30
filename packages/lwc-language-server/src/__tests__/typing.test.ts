9be324acce4e010b60160904d45df03ff4fc169d
Micah Cooper <micah+github@mrmicahcooper.com>
2020-05-29 21:30:01 -0400

    Add types in tests

--- packages/lwc-language-server/src/__tests__/typing.test.ts
+++ packages/lwc-language-server/src/__tests__/typing.test.ts
@@ -2,7 +2,7 @@ import Typing from '../typing';
 
 describe('Typing.declaration', () => {
     it('generates the typing declaration for a content asset file.', () => {
-        const declaration = Typing.declaration('logo.asset-meta.xml');
+        const declaration: string = Typing.declaration('logo.asset-meta.xml');
         const expectedDeclaration: string = `declare module "@salesforce/contentAssetUrl/logo" {
     var logo: string;
     export default logo;
@@ -13,7 +13,7 @@ describe('Typing.declaration', () => {
     });
 
     it('handls a full path', () => {
-        const declaration = Typing.declaration('logo.asset-meta.xml');
+        const declaration: string = Typing.declaration('logo.asset-meta.xml');
         const expectedDeclaration: string = `declare module "@salesforce/contentAssetUrl/logo" {
     var logo: string;
     export default logo;
@@ -24,7 +24,7 @@ describe('Typing.declaration', () => {
     });
 
     it('generate the typing declaration for a static resource file', () => {
-        const declaration = Typing.declaration('d3.resource-meta.xml');
+        const declaration: string = Typing.declaration('d3.resource-meta.xml');
         const expectedDeclaration: string = `declare module "@salesforce/resourceUrl/d3" {
     var d3: string;
     export default d3;
@@ -35,7 +35,7 @@ describe('Typing.declaration', () => {
     });
 
     it('generate the typing declaration for a message channels file', () => {
-        const declaration = Typing.declaration('Channel1.messageChannel-meta.xml');
+        const declaration: string = Typing.declaration('Channel1.messageChannel-meta.xml');
         const expectedDeclaration = `declare module "@salesforce/messageChannel/Channel1__c" {
     var Channel1: string;
     export default Channel1;
@@ -46,14 +46,14 @@ describe('Typing.declaration', () => {
     });
 
     it('throws an error if the wrong -meta.xml file is passed in', () => {
-        const filename = 'asset.foobar-meta.xml';
+        const filename: string = 'asset.foobar-meta.xml';
         expect(() => {
             Typing.declaration(filename);
         }).toThrow();
     });
 
     it('handles a full path', () => {
-        const declaration = Typing.declaration('./foo/bar/buz/logo.asset-meta.xml');
+        const declaration: string = Typing.declaration('./foo/bar/buz/logo.asset-meta.xml');
         const expectedDeclaration: string = `declare module "@salesforce/contentAssetUrl/logo" {
     var logo: string;
     export default logo;
