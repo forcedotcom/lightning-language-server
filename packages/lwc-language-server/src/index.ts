70ff02ccc10545b31f208c969a052bb973b19f55
Micah Cooper <micah+github@mrmicahcooper.com>
2020-06-03 13:46:04 -0400

    Return paths to indexed typings

--- packages/lwc-language-server/src/index.ts
+++ packages/lwc-language-server/src/index.ts
@@ -1,17 +1,37 @@
 import * as glob from 'glob';
 import * as path from 'path';
+import { shared } from '@salesforce/lightning-lsp-common';
+const { detectWorkspaceHelper, WorkspaceType } = shared;
 
 export default class Index {
     readonly workspaceRoot: string;
     readonly sfdxPackageDirsPattern: string;
+    readonly projectType: number;
 
-    constructor(attributes: any) {
+    constructor(attributes: { [key: string]: string }) {
         this.workspaceRoot = path.resolve(attributes.workspaceRoot);
         this.sfdxPackageDirsPattern = attributes.sfdxPackageDirsPattern;
+        this.projectType = detectWorkspaceHelper(attributes.workspaceRoot);
     }
 
-    metaFilePaths() {
-        const metaFileGlob = `${this.sfdxPackageDirsPattern}/**/+(staticresources|contentassets|messageChannels)/*.+(resource|asset|messageChannel)-meta.xml`;
-        return glob.sync(metaFileGlob, { cwd: this.workspaceRoot });
+    metaFilePaths(): string[] {
+        const globString = `${this.sfdxPackageDirsPattern}/**/+(staticresources|contentassets|messageChannels)/*.+(resource|asset|messageChannel)-meta.xml`;
+        return glob.sync(globString, { cwd: this.workspaceRoot });
+    }
+
+    metaFileTypingPaths(): string[] {
+        const globString = `${this.typingsDir()}/+(messageChannels|staticresources|contentassets)/**/*.d.ts`;
+        return glob.sync(globString, { cwd: this.workspaceRoot });
+    }
+
+    typingsDir() {
+        switch (this.projectType) {
+            case WorkspaceType.SFDX:
+                return '.sfdx/typings/lwc';
+            case WorkspaceType.CORE_PARTIAL:
+                return '../.vscode/typings/lwc';
+            case WorkspaceType.CORE_ALL:
+                return '.vscode/typings/lwc';
+        }
     }
 }
