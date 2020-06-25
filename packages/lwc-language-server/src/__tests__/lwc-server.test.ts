5c77eabf74341f03918575aae82881fd5c9ed54d
Micah Cooper <micah+github@mrmicahcooper.com>
2020-06-25 00:33:41 -0400

    Organize lwc-server.test.ts

--- packages/lwc-language-server/src/__tests__/lwc-server.test.ts
+++ packages/lwc-language-server/src/__tests__/lwc-server.test.ts
@@ -10,6 +10,8 @@ const filename = '../../test-workspaces/sfdx-workspace/force-app/main/default/lw
 const uri = URI.parse(filename).fsPath;
 const document: TextDocument = TextDocument.create(uri, 'html', 0, fsExtra.readFileSync(filename).toString());
 
+const server: Server = new Server();
+
 jest.mock('vscode-languageserver', () => {
     const actual = jest.requireActual('vscode-languageserver');
     return {
@@ -36,24 +38,14 @@ jest.mock('vscode-languageserver', () => {
     };
 });
 
-describe('findDynamicContent', () => {
-    const text: string = '{foobar}, {foo.bar} so\nmething {baz.bux}';
-
-    it('returns the dynamic match at the given offset if it exists', () => {
-        expect(findDynamicContent(text, 5)).toEqual('foobar');
-    });
-
-    it('returns the match if its not the only one in the string', () => {
-        expect(findDynamicContent(text, 12)).toEqual('foo');
-    });
-
-    it('returns null when not on dynamic content', () => {
-        expect(findDynamicContent(text, 25)).toBeNull();
+describe('new', () => {
+    it('creates a new instance', () => {
+        expect(server.connection);
+        expect(server.documents);
     });
 });
 
-describe('new', () => {
-    const server: Server = new Server();
+describe('handlers', () => {
     const initializeParams: InitializeParams = {
         processId: 0,
         rootUri: '',
@@ -65,31 +57,6 @@ describe('new', () => {
             },
         ],
     };
-
-    it('creates a new instance', () => {
-        expect(server.connection);
-        expect(server.documents);
-    });
-
-    describe('capabilities', () => {
-        it('returns what the server can do', () => {
-            expect(server.capabilities).toEqual({
-                capabilities: {
-                    textDocumentSync: 'html',
-                    completionProvider: {
-                        resolveProvider: true,
-                    },
-                    hoverProvider: true,
-                    definitionProvider: true,
-                    workspace: {
-                        workspaceFolders: {
-                            supported: true,
-                        },
-                    },
-                },
-            });
-        });
-    });
 
     describe('#onCompletion', () => {
         it('returns a list of available completion items', async () => {
@@ -109,56 +76,94 @@ describe('new', () => {
             expect(labels).toInclude('div'); // also includes normal html tags
         });
     });
+});
 
-    describe('cursorInfo', () => {
-        it('it knows when Im in a start tag', () => {
-            const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 16, character: 23 } }, document);
-            expect(cursorInfo).toEqual({ type: Token.Tag, name: 'c-todo_item', tag: 'c-todo_item' });
-        });
-
-        it('knows when Im on an attribute name', () => {
-            const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 17, character: 26 } }, document);
-            expect(cursorInfo).toEqual({ type: Token.AttributeKey, name: 'key', tag: 'c-todo_item' });
-        });
-
-        it('knows when Im on a dynamic attribute value (inside "{}")', () => {
-            const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 18, character: 33 } }, document);
-            expect(cursorInfo.type).toEqual(Token.DynamicAttributeValue);
-            expect(cursorInfo.name).toEqual('todo');
-            expect(cursorInfo.tag).toEqual('c-todo_item');
-            expect(cursorInfo.range).toEqual({
-                start: {
-                    character: 60,
-                    line: 15,
+describe('#capabilities', () => {
+    it('returns what the server can do', () => {
+        expect(server.capabilities).toEqual({
+            capabilities: {
+                textDocumentSync: 'html',
+                completionProvider: {
+                    resolveProvider: true,
                 },
-                end: {
-                    character: 66,
-                    line: 15,
+                hoverProvider: true,
+                definitionProvider: true,
+                workspace: {
+                    workspaceFolders: {
+                        supported: true,
+                    },
                 },
-            });
+            },
         });
+    });
+});
 
-        it('knows when Im on an attribute value', () => {
-            const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 7, character: 35 } }, document);
-            expect(cursorInfo).toEqual({ type: Token.AttributeValue, name: '"off"', tag: 'input' });
-        });
+describe('#cursorInfo', () => {
+    server.languageService = getLanguageService();
 
-        it('knows when Im in content', () => {
-            const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 37, character: 24 } }, document);
-            expect(cursorInfo.type).toEqual(Token.Content);
-            expect(cursorInfo.tag).toEqual('button');
-        });
+    it('knows when Im in a start tag', () => {
+        const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 16, character: 23 } }, document);
+        expect(cursorInfo).toEqual({ type: Token.Tag, name: 'c-todo_item', tag: 'c-todo_item' });
+    });
 
-        it('knows when Im in dynamic content', () => {
-            const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 27, character: 68 } }, document);
-            expect(cursorInfo.type).toEqual(Token.DynamicContent);
-            expect(cursorInfo.tag).toEqual('strong');
-        });
+    it('knows when Im on an attribute name', () => {
+        const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 17, character: 26 } }, document);
+        expect(cursorInfo).toEqual({ type: Token.AttributeKey, name: 'key', tag: 'c-todo_item' });
+    });
 
-        it('knows when Im not dynamic content', () => {
-            const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 27, character: 76 } }, document);
-            expect(cursorInfo.type).toEqual(Token.Content);
-            expect(cursorInfo.tag).toEqual('strong');
+    it('knows when Im on a dynamic attribute value (inside "{}")', () => {
+        const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 18, character: 33 } }, document);
+        expect(cursorInfo.type).toEqual(Token.DynamicAttributeValue);
+        expect(cursorInfo.name).toEqual('todo');
+        expect(cursorInfo.tag).toEqual('c-todo_item');
+        expect(cursorInfo.range).toEqual({
+            start: {
+                character: 60,
+                line: 15,
+            },
+            end: {
+                character: 66,
+                line: 15,
+            },
         });
+    });
+
+    it('knows when Im on an attribute value', () => {
+        const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 7, character: 35 } }, document);
+        expect(cursorInfo).toEqual({ type: Token.AttributeValue, name: '"off"', tag: 'input' });
+    });
+
+    it('knows when Im in content', () => {
+        const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 37, character: 24 } }, document);
+        expect(cursorInfo.type).toEqual(Token.Content);
+        expect(cursorInfo.tag).toEqual('button');
+    });
+
+    it('knows when Im in dynamic content', () => {
+        const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 27, character: 68 } }, document);
+        expect(cursorInfo.type).toEqual(Token.DynamicContent);
+        expect(cursorInfo.tag).toEqual('strong');
+    });
+
+    it('knows when Im not dynamic content', () => {
+        const cursorInfo = server.cursorInfo({ textDocument: { uri }, position: { line: 27, character: 76 } }, document);
+        expect(cursorInfo.type).toEqual(Token.Content);
+        expect(cursorInfo.tag).toEqual('strong');
+    });
+});
+
+describe('findDynamicContent', () => {
+    const text: string = '{foobar}, {foo.bar} so\nmething {baz.bux}';
+
+    it('returns the dynamic match at the given offset if it exists', () => {
+        expect(findDynamicContent(text, 5)).toEqual('foobar');
+    });
+
+    it('returns the match if its not the only one in the string', () => {
+        expect(findDynamicContent(text, 12)).toEqual('foo');
+    });
+
+    it('returns null when not on dynamic content', () => {
+        expect(findDynamicContent(text, 25)).toBeNull();
     });
 });
