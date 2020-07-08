79a17ff2790c5dffab91ae05bac1fb2913396d25
Ruben Manrique <miwialex@gmail.com>
2020-07-08 14:42:07 -0500

    change Tag.uri to use toString

--- packages/lwc-language-server/src/tag.ts
+++ packages/lwc-language-server/src/tag.ts
@@ -102,7 +102,7 @@ export default class Tag implements ITagData {
     }
 
     get uri() {
-        return URI.parse(path.resolve(this.file)).fsPath;
+        return URI.file(path.resolve(this.file)).toString();
     }
 
     get allAttributes() {
