a5896037aa95e07c7e69d8ae6a0b388d9cd389d8
Rodrigo Lopetegui <rlopetegui@salesforce.com>
2020-09-04 16:25:16 -0300

    Changes that were not merged

--- packages/aura-language-server/src/tern-server/tern-aura.ts
+++ packages/aura-language-server/src/tern-server/tern-aura.ts
@@ -74,25 +74,6 @@ async function readFile(filename) {
     }
 }
 
-<<<<<<< HEAD
-=======
-function parent(path) {
-    const splits = path.split('/');
-    if (splits.size === 1) {
-        return '';
-    }
-    return splits[splits.length - 3];
-}
-
-function dirName(path) {
-    const lastSlash = path.lastIndexOf('/');
-    if (lastSlash === -1) {
-        return '';
-    }
-    return path.slice(0, lastSlash + 1);
-}
-
->>>>>>> 004aa976a50625e1b48f9a6701d230291d1d06c4
 function baseName(path) {
     const lastSlash = path.lastIndexOf('/');
     if (lastSlash === -1) {
@@ -123,103 +104,7 @@ function initScope(scope) {
     // @ts-ignore
     exports.propagate(moduleExports, WG_DEFAULT_EXPORT);
 }
-<<<<<<< HEAD
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function _debug(log) {
-    //console.log(log);
-}
-
-function getName(name, type) {
-    const newname = name.replace(/Controller.js$|Helper.js$|Renderer.js$|Test.js$/, '') + type;
-    return newname;
-}
-
-function getController(name) {
-    return getName(name, 'Controller.js');
-}
-
-function getHelper(name) {
-    return getName(name, 'Helper.js');
-}
-
-function getRenderer(name) {
-    return getName(name, 'Renderer.js');
-}
-
-function resolver(file, parent) {
-    return file;
-}
-
-function unloadDefs() {
-    // @ts-ignore
-    server.deleteDefs('Aura');
-=======
-async function getLibraryIncludes(file, library) {
-    const libFile = await getLibFile(file, library);
-    const inc = await getLibIncludes(getFilename(libFile));
-    const includes = [];
-    const bn = dirName(libFile);
-    // @ts-ignore
-    inc.forEach(function(name) {
-        const fname = bn + name + '.js';
-        if (!isBlocklisted(fname)) {
-            includes.push(fname);
-        }
-    });
-    return includes;
->>>>>>> 004aa976a50625e1b48f9a6701d230291d1d06c4
-}
-
-function readFileAsync(filename, c) {
-    readFile(filename).then(function(contents) {
-        c(null, contents);
-    });
-}
-
-<<<<<<< HEAD
-function loadDefs() {
-    let defs = fs.readFileSync(path.join(__dirname, 'aura_types.json'), 'utf8');
-    defs = JSON.parse(defs);
-    // @ts-ignore
-    server.addDefs(defs);
-}
-
-function findAndBindComponent(type, server, cx, infer) {
-    const evs = cx.props['Component'];
-    if (!evs) {
-        return;
-    }
-    for (let z = 0; z < evs.length; z++) {
-        const y = evs[z];
-        if (y.name === 'Aura.Component') {
-            const obj = y.props['Component'].types[0].props['prototype'].getObjType();
-            const int = infer.getInstance(obj);
-            int.propagate(type);
-        }
-    }
-}
-
-function findAndBindHelper(type, server, modules, file) {
-    const helperFile = getHelper(file.name);
-
-    const bn = trimExt(baseName(helperFile));
-    const r = server.findFile(helperFile);
-    if (!r) {
-        server.addFile(helperFile);
-    }
-    const helper = modules.resolveModule(helperFile);
-    //  console.log("Resolved module" + helperFile);
-    //  console.dir(helper);
-    const hp = helper.getProp(bn);
-    if (!hp.getType()) {
-        // this handles new props added to the helper...
-        helper.on('addType', function(helperType, val) {
-            const p = new ForAllProps_Purgeable(function(prop, val, local) {
-                if (bn === prop) {
-                    val.propagate(type);
-=======
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function _debug(log) {
@@ -265,296 +150,21 @@ function loadDefs() {
     server.addDefs(defs);
 }
 
-async function processIfLibrary(file, modules) {
-    let lib = await getLibraryForJS(file.name);
-    // @ts-ignore
-    const libs = lib ? [lib] : [];
-
-    if (libs.length > 0) {
-        _debug('Process libs of: ' + file.name);
-        const bn = trimExt(baseName(file.name));
-        const ln = trimExt(baseName(libs[0]));
-        const libName = getFilename(libs[0]);
-        const imps = await getLibImports(libName, bn);
-
-        // use alternate module for libs
-        const ns = parent(dirName(file.name));
-        const l = modules.resolveModule('mod:' + ns + ':' + ln, file.name);
-        let outObj;
-        //console.log("__ the basename "+baseName(libs[0]) );
-        if (!l.getType()) {
-            //console.log("Created main entry for library");
-            outObj = await newObj();
-            outObj.origin = 'Aura';
-            outObj.name = baseName(libs[0]);
-            l.addType(outObj);
-        } else {
-            //console.log("Found main entry for library: "+l.getType() + " "+l.getType().name + " " + l.getType().origin);
-            outObj = l.getType();
-        }
-        // @ts-ignore
-        if (imps) {
-            _debug('Process imported libs of: ' + file.name);
-            // get fn decl
-            const fn = file.ast.body[0];
-            const state = fn.scope;
-            // bind included imports to library function....
-            // @ts-ignore
-            const imports = imps.split(',');
-
-            const libfilesResolved = [];
-            const imfs = [];
-            const importedModules = [];
-            for (let i = 0; i < imports.length; i++) {
-                // resolve....
-                let importedModule = imports[i].trim();
-                if (!importedModule) {
-                    continue;
-                }
-                if (importedModule.indexOf(':') > -1) {
-                    const splits = importedModule.split(':');
-                    const qn = splits[0] + ':' + splits[1];
-                    const libfile = await getLibFile(file.name, qn);
-                    // @ts-ignore
-                    if (!libfile) {
-                        continue;
-                    }
-
-                    lib = modules.resolveModule('mod:' + qn);
-                    // @ts-ignore
-                    if (!lib.getType()) {
-                        const zz = await newObj();
-                        // @ts-ignore
-                        zz.origin = libfile;
-                        // @ts-ignore
-                        zz.name = baseName(libfile);
-                        // @ts-ignore
-                        lib.addType(zz);
-                    }
-                    libfilesResolved.push(modules.resolveModule('mod:' + qn));
-                    importedModule = splits[2];
-                    imfs.push(dirName(libfile) + importedModule + '.js');
-                } else {
-                    imfs.push(dirName(file.name) + importedModule + '.js');
-                }
-                importedModules.push(importedModule);
-            }
-            // re-establsh context after awaits
-            // @ts-ignore
-            infer.withContext(server.cx, function() {
-                for (let i = 0; i < libfilesResolved.length; i++) {
-                    const importedModule = importedModules[i];
-                    const lib = libfilesResolved[i];
-
-                    const pm = state.fnType.args[i];
-                    if (!pm || pm.getType(false)) {
-                        continue;
-                    }
-                    if (lib.getType().hasProp(importedModule)) {
-                        try {
-                            pm.addType(
-                                lib
-                                    .getType()
-                                    .getProp(importedModule)
-                                    .getType(),
-                            );
-                        } catch (zzz) {}
-                    } else {
-                        const pname = importedModule;
-                        // so, in effect this isn't really used, since (at least tern.ide)
-                        // calls content assist frequently that by the time this called back,
-                        // the file will be reindex, and the other lib file would have already
-                        /// been loaded.
-                        lib.getType().on(
-                            'addProp',
-                            function(pmType, prop, val) {
-                                if (pname === prop) {
-                                    pmType.addType(val);
-                                }
-                            }.bind(this, pm),
-                        );
-                    }
-                }
-            });
-        }
-        // re-establsh context after awaits
-        // @ts-ignore
-        infer.withContext(server.cx, function() {
-            _debug('Process exported libs of: ' + file.name);
-            walk.simple(
-                file.ast,
-                {
-                    ReturnStatement: function(node, state) {
-                        try {
-                            // @ts-ignore
-                            const parent = infer.parentNode(node, file.ast);
-                            // @ts-ignore
-                            const grand = infer.parentNode(parent, file.ast);
-                            // @ts-ignore
-                            const great = infer.parentNode(grand, file.ast);
-                            if (great && great['type'] === 'Program') {
-                                if (node.argument) {
-                                    if (node.argument['type'] === 'Identifier') {
-                                        const t = state.getProp(node.argument.name);
-                                        if (t) {
-                                            const exported = t.getObjType();
-                                            outObj.defProp(bn, node.argument).addType(exported);
-                                        }
-                                    } else if (node.argument['type'] === 'ObjectExpression') {
-                                        outObj.defProp(bn, node.argument).addType(node.argument.objType);
-                                    } else if (node.argument['type'] === 'FunctionExpression') {
-                                        outObj.defProp(bn, node.argument).addType(state.fnType.getType());
-                                    } else if (node.argument['type'] === 'CallExpression') {
-                                        outObj.defProp(bn, node.argument).addType(state.getType());
-                                    }
-                                }
-                            }
-                        } catch (ignore) {
-                            console.error(ignore);
-                        }
-                    },
-                },
-                // @ts-ignore
-                infer.searchVisitor,
-            );
-        });
-    }
-}
-async function processIfComponent(file, modules) {
-    //        console.log("file " + file.name);
-    //        console.log("cmp " + jsToCmp[file.name]);
-
-    const cmp = await getComponentForJS(file.name);
-    // @ts-ignore
-    const cmps = cmp ? [cmp] : [];
-
-    if (cmps.length > 0) {
-        _debug('Discover libs of: ' + cmps[0]);
-        const ins = await getCmpImports(getFilename(cmps[0]));
-        const libs = [];
-        // @ts-ignore
-        for (let i = 0; i < ins.length; i++) {
-            const an_import = ins[i];
-            const library = an_import.library;
-            const libfile = await getLibFile(file.name, library);
-            libs.push(libfile);
-        }
-        for (let i = 0; i < libs.length; i++) {
-            const library = ins[i].library;
-            const libfile = libs[i];
-            if (!libfile) {
-                continue;
-            }
-            const lib = modules.resolveModule('mod:' + library, libfile);
-            //console.log("Ensure lib added...")
-            //console.log("Resolved: "+lib);
-            if (!lib.getType()) {
-                // console.log("no type")
-                const zz = await newObj();
-                // @ts-ignore
-                zz.origin = libfile;
-                // @ts-ignore
-                zz.name = baseName(libfile);
-                if (!lib.getType()) {
-                    // recheck, after awaits
-                    lib.addType(zz);
-                }
-                const inc = await getLibraryIncludes(file.name, library);
-                for (let j = 0; j < inc.length; j++) {
-                    // @ts-ignore
-                    if (!server.findFile(inc[j])) {
-                        // @ts-ignore
-                        server.addFile(inc[j]);
-                        // console.log("Added lib dep: "+inc[i]);
-                    } else {
-                        // console.log("File found");
-                    }
-                }
-            }
-        }
-
-        // reestablish-context after awaits
-        // @ts-ignore
-        infer.withContext(server.cx, function() {
-            for (let m = 0; m < libs.length; m++) {
-                const library = ins[m].library;
-                const libfile = libs[m];
-                if (!libfile) {
-                    continue;
-                }
-                const property = ins[m].property;
-                const lib = modules.resolveModule('mod:' + library, libfile);
-                try {
-                    walk.simple(
-                        file.ast,
-                        {
-                            FunctionExpression: function(node, state) {
-                                //console.log("Bound to this...");
-                                const ss = node.scope;
-                                const con = ss && ss.fnType && ss.fnType.self.getType();
-                                if (con) {
-                                    lib.getType().propagate(con.defProp(property));
-                                }
-                            },
-                        },
-                        // @ts-ignore
-                        infer.searchVisitor,
-                    );
-                } catch (ignore) {
-                    console.error(ignore);
->>>>>>> 004aa976a50625e1b48f9a6701d230291d1d06c4
-                }
-            });
-            p.addSource(type);
-            helperType.propagate(p);
-        });
-    } else {
-        // now we need to handle there were changes to the .cmp,
-        // but not the helper,
-        const p = new ForAllProps_Purgeable(function(prop, val, local) {
-            if (bn === prop) {
-                val.propagate(type);
-            }
-        });
-        p.addSource(type);
-        helper.propagate(p);
-    }
-}
-
-<<<<<<< HEAD
-function findAndBindEvent(type, server, cx, infer) {
-    // this is slightly hacky, but have no idea how to get the event Otherwise
-    const evs = cx.props['Event'];
-=======
 function findAndBindComponent(type, server, cx, infer) {
     const evs = cx.props['Component'];
->>>>>>> 004aa976a50625e1b48f9a6701d230291d1d06c4
     if (!evs) {
         return;
     }
     for (let z = 0; z < evs.length; z++) {
         const y = evs[z];
-<<<<<<< HEAD
-        if (y.name === 'Aura.Event') {
-            const obj = y.props['Event'].types[0].props['prototype'].getObjType();
-=======
         if (y.name === 'Aura.Component') {
             const obj = y.props['Component'].types[0].props['prototype'].getObjType();
->>>>>>> 004aa976a50625e1b48f9a6701d230291d1d06c4
             const int = infer.getInstance(obj);
             int.propagate(type);
         }
     }
 }
 
-<<<<<<< HEAD
-function ternError(msg) {
-    const err = new Error(msg);
-    err.name = 'TernError';
-    return err;
-}
-
-=======
 function findAndBindHelper(type, server, modules, file) {
     const helperFile = getHelper(file.name);
 
@@ -613,7 +223,6 @@ function ternError(msg) {
     return err;
 }
 
->>>>>>> 004aa976a50625e1b48f9a6701d230291d1d06c4
 async function connectModule(file, out) {
     if (isBlocklisted(file.name)) {
         return;
