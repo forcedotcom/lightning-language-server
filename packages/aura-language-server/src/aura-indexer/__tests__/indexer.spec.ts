6f4d37ece746d133f12983eb20aa7260b2ec0d3d
Rodrigo Lopetegui <rlopetegui@salesforce.com>
2020-09-22 11:21:11 -0300

    Added other comment, to see if second try breaks the  flow

--- packages/aura-language-server/src/aura-indexer/__tests__/indexer.spec.ts
+++ packages/aura-language-server/src/aura-indexer/__tests__/indexer.spec.ts
@@ -57,6 +57,7 @@ it('aura indexer', async () => {
     const sortedTags = new Map([...tags.entries()].sort());
     //TODO Delete this comment
     // This is a change so the  PR can be uploaded and compared
+    // This other  comment should not break the flow
     expect(sortedTags).toMatchSnapshot();
 
     const namespaces = auraIndexer.getAuraNamespaces().sort();
