dd7214b9c77d44afc604356d69682844f2d14a45
Luis Campos <lcamposguajardo@gmail.com>
2020-09-30 13:39:41 -0700

    fix: add { and . as trigger characters

--- packages/lwc-language-server/src/__tests__/lwc-server.test.ts
+++ packages/lwc-language-server/src/__tests__/lwc-server.test.ts
@@ -220,7 +220,7 @@ describe('#capabilities', () => {
                 textDocumentSync: 'html',
                 completionProvider: {
                     resolveProvider: true,
-                    triggerCharacters: ['-', '_', '<', '"', '=', '/', '>'],
+                    triggerCharacters: ['{', '.', '-', '_', '<', '"', '=', '/', '>'],
                 },
                 hoverProvider: true,
                 definitionProvider: true,
