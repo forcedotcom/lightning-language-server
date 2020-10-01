014446c964fe5425ce4fa4615baf1907e65e39a4
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-02 00:53:03 +0100

    TEST edge cases for string-util methods

--- packages/aura-language-server/src/tern-server/__tests__/string-util.test.ts
+++ packages/aura-language-server/src/tern-server/__tests__/string-util.test.ts
@@ -5,7 +5,17 @@ describe('string-util', () => {
     it('should find start and end of word', () => {
       const testWord = 'test';
       const testString: string = `   ${testWord}   `;
-      const result = findWord(testString, testString.length / 2);
+      const result = findWord(testString, Math.floor(testString.length / 2));
+
+      expect(result).toBeDefined();
+      expect(result.start).toEqual(testString.indexOf(testWord));
+      expect(result.end).toEqual(testString.indexOf(testWord) + testWord.length);
+    });
+
+    it('should find start and end of word with number and underscore', () => {
+      const testWord = 'test_1';
+      const testString: string = `   ${testWord}   `;
+      const result = findWord(testString, Math.floor(testString.length / 2));
 
       expect(result).toBeDefined();
       expect(result.start).toEqual(testString.indexOf(testWord));
@@ -15,7 +25,7 @@ describe('string-util', () => {
     it('should find start and end of word in multiline string', () => {
       const testWord = 'test';
       const testString: string = '\n\n' + testWord + '\n\n';
-      const result = findWord(testString, testString.length / 2);
+      const result = findWord(testString, Math.floor(testString.length / 2));
 
       expect(result).toBeDefined();
       expect(result.start).toEqual(testString.indexOf(testWord) - 1);
@@ -28,7 +38,7 @@ describe('string-util', () => {
       const testWord = 'test';
       const commas = ',,,';
       const testString: string = `${commas}${testWord}`;
-      const result = countPreviousCommas(testString, commas.length + testString.length / 2);
+      const result = countPreviousCommas(testString, commas.length + Math.floor(testString.length / 2));
 
       expect(result).toBeDefined();
       expect(result).toEqual(commas.length);
@@ -38,10 +48,20 @@ describe('string-util', () => {
       const testWord = 'test';
       const threeCommaString = 'value1, value2, value3, ';
       const testString: string = `${threeCommaString}${testWord}`;
-      const result = countPreviousCommas(testString, threeCommaString.length + testString.length / 2);
+      const result = countPreviousCommas(testString, threeCommaString.length + Math.floor(testString.length / 2));
 
       expect(result).toBeDefined();
       expect(result).toEqual(3);
+    });
+
+    it('should find number of preceding commas with new line', () => {
+      const testWord = 'test';
+      const commaString = ',\n,, ';
+      const testString: string = `${commaString}${testWord}`;
+      const result = countPreviousCommas(testString, commaString.length + Math.floor(testString.length / 2));
+
+      expect(result).toBeDefined();
+      expect(result).toEqual(2);
     });
   });
 
@@ -49,7 +69,7 @@ describe('string-util', () => {
     it('should find index of preceding left bracket', () => {
       const testWord = 'test';
       const testString: string = `(${testWord})`;
-      const result = findPreviousLeftParan(testString, testString.length / 2);
+      const result = findPreviousLeftParan(testString, Math.floor(testString.length / 2));
 
       expect(result).toBeDefined();
       expect(result).toEqual(testString.indexOf('('));
@@ -58,10 +78,19 @@ describe('string-util', () => {
     it('should find index of preceding left bracket with chars between', () => {
       const testWord = 'test';
       const testString: string = `func(anotherParameter, ${testWord})`;
-      const result = findPreviousLeftParan(testString, testString.length / 2);
+      const result = findPreviousLeftParan(testString, Math.floor(testString.length / 2));
 
       expect(result).toBeDefined();
       expect(result).toEqual(testString.indexOf('('));
+    });
+
+    it('should return -1 with new line', () => {
+      const testWord = 'test';
+      const testString: string = `(\n${testWord})`;
+      const result = findPreviousLeftParan(testString, Math.floor(testString.length / 2));
+
+      expect(result).toBeDefined();
+      expect(result).toEqual(-1);
     });
   });
 
@@ -75,6 +104,41 @@ describe('string-util', () => {
       expect(result).toBeDefined();
       expect(result.start).toEqual(testString.indexOf(perviousWord));
       expect(result.end).toEqual(testString.indexOf(perviousWord) + perviousWord.length + 1);
+    });
+
+    it('should find new line char before "." with new line', () => {
+      const testWord = 'test';
+      const perviousWord = 'word';
+      const testString: string = `   ${perviousWord}\n.${testWord}   `;
+      const result = findPreviousWord(testString, testString.indexOf(testWord) + 2);
+
+      expect(result).toBeDefined();
+      expect(result.start).toEqual(testString.indexOf('\n'));
+      expect(result.end).toEqual(testString.indexOf(testWord));
+    });
+
+    it('should return index of start char without "."', () => {
+      const testWord = 'test';
+      const perviousWord = 'word';
+      const testString: string = `   ${perviousWord} ${testWord}   `;
+      const offset = testString.indexOf(testWord) + 2;
+      const result = findPreviousWord(testString, offset);
+
+      expect(result).toBeDefined();
+      expect(result.start).toEqual(offset);
+      expect(result.end).toEqual(offset);
+    });
+
+    it('should return index previous word after ","', () => {
+      const testWord = 'test';
+      const perviousWord = 'word';
+      const testString: string = `   ,${perviousWord}.${testWord}\n   `;
+      const offset = testString.indexOf(testWord) + 2;
+      const result = findPreviousWord(testString, offset);
+
+      expect(result).toBeDefined();
+      expect(result.start).toEqual(testString.indexOf(perviousWord));
+      expect(result.end).toEqual(testString.indexOf(testWord));
     });
   });
 });
