05bef9101c398af843c8c4111bfc20d95b2f37cc
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-01 23:14:00 +0100

    TEST countPreviousCommas method in string-utils

--- packages/aura-language-server/src/tern-server/__tests__/string-util.test.ts
+++ packages/aura-language-server/src/tern-server/__tests__/string-util.test.ts
@@ -1,4 +1,4 @@
-import {findWord} from '../string-util'
+import {countPreviousCommas, findWord} from '../string-util'
 
 describe('string-util', () => {
   describe('findWord', () => {
@@ -20,6 +20,28 @@ describe('string-util', () => {
       expect(result).toBeDefined();
       expect(result.start).toEqual(testString.indexOf(testWord) - 1);
       expect(result.end).toEqual(testString.indexOf(testWord) + testWord.length);
+    });
+  });
+
+  describe('countPreviousCommas', () => {
+    it('should find number of preceding commas', () => {
+      const testWord = 'test';
+      const commas = ',,,';
+      const testString: string = `${commas}${testWord}`;
+      const result = countPreviousCommas(testString, commas.length + testString.length / 2);
+
+      expect(result).toBeDefined();
+      expect(result).toEqual(commas.length);
+    });
+
+    it('should find number of preceding commas with chars between', () => {
+      const testWord = 'test';
+      const threeCommaString = 'value1, value2, value3, ';
+      const testString: string = `${threeCommaString}${testWord}`;
+      const result = countPreviousCommas(testString, threeCommaString.length + testString.length / 2);
+
+      expect(result).toBeDefined();
+      expect(result).toEqual(3);
     });
   });
 });
