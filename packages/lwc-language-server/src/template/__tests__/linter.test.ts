369427189dcfc18d8f10de516d886b8ebc564985
Nolan Lawson <nlawson@salesforce.com>
2022-05-12 15:18:26 -0700

    test: fix test

--- packages/lwc-language-server/src/template/__tests__/linter.test.ts
+++ packages/lwc-language-server/src/template/__tests__/linter.test.ts
@@ -6,19 +6,14 @@ it('returns a list of all the template compilation errors', () => {
     const document = TextDocument.create('test://test.html', 'html', 0, content);
 
     const diagnostics = templateLinter(document);
-    expect(diagnostics).toHaveLength(3);
+    expect(diagnostics).toHaveLength(2);
     expect(diagnostics[0].message).toMatch(/If directive should be an expression/);
     expect(diagnostics[0].range).toMatchObject({
         start: { character: 20 },
         end: { character: 47 },
     });
-    expect(diagnostics[1].message).toMatch(/Invalid expression/);
+    expect(diagnostics[1].message).toMatch(/<lighting- is not a valid namespace, sure you didn't mean "<lightning-"?/);
     expect(diagnostics[1].range).toMatchObject({
-        start: { character: 48 },
-        end: { character: 63 },
-    });
-    expect(diagnostics[2].message).toMatch(/<lighting- is not a valid namespace, sure you didn't mean "<lightning-"?/);
-    expect(diagnostics[2].range).toMatchObject({
         start: { character: 74 },
         end: { character: 84 },
     });
