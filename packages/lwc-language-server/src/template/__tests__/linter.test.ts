09c6cf65ef1a61f269128cb94478d1736d92766d
Nolan Lawson <nlawson@salesforce.com>
2022-05-12 15:24:52 -0700

    test: better fix for test

--- packages/lwc-language-server/src/template/__tests__/linter.test.ts
+++ packages/lwc-language-server/src/template/__tests__/linter.test.ts
@@ -2,18 +2,23 @@ import { TextDocument } from 'vscode-languageserver';
 import templateLinter from '../linter';
 
 it('returns a list of all the template compilation errors', () => {
-    const content = `<template><template if:true="invalidExpression">{Math.random()}</template><lighting-card></lighting-card></template>`;
+    const content = '<template><template if:true="invalidExpression"></template>{Math.random()}<lighting-card></lighting-card></template>';
     const document = TextDocument.create('test://test.html', 'html', 0, content);
 
     const diagnostics = templateLinter(document);
-    expect(diagnostics).toHaveLength(2);
+    expect(diagnostics).toHaveLength(3);
     expect(diagnostics[0].message).toMatch(/If directive should be an expression/);
     expect(diagnostics[0].range).toMatchObject({
         start: { character: 20 },
         end: { character: 47 },
     });
-    expect(diagnostics[1].message).toMatch(/<lighting- is not a valid namespace, sure you didn't mean "<lightning-"?/);
+    expect(diagnostics[1].message).toMatch(/Invalid expression/);
     expect(diagnostics[1].range).toMatchObject({
+        start: { character: 59 },
+        end: { character: 74 },
+    });
+    expect(diagnostics[2].message).toMatch(/<lighting- is not a valid namespace, sure you didn't mean "<lightning-"?/);
+    expect(diagnostics[2].range).toMatchObject({
         start: { character: 74 },
         end: { character: 84 },
     });
