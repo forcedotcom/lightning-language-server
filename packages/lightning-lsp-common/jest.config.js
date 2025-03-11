598215683f849ca489cce7279e274c5c50d8f425
Cristina Cañizales <113132642+CristiCanizales@users.noreply.github.com>
2025-03-10 21:20:57 -0300

    chore(W-16301365): update node to v20 + bunch of deps (#606)
    * chore: node update
    
    * chore: update nvmrc file
    
    * chore: some more dependencies bumps
    
    * chore: some more dependencies bumps part 2
    
    * chore: some more dependencies bumps part 3
    
    * chore: some more dependencies bumps part 4 - glob, jest, shell
    
    * chore: undo glob bump. ugh windows

--- packages/lightning-lsp-common/jest.config.js
+++ packages/lightning-lsp-common/jest.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    displayName: 'unit',
-    transform: {
-        ".ts": "ts-jest"
-      },
-      testRegex: 'src/.*(\\.|/)(test|spec)\\.(ts|js)$',
-      moduleFileExtensions: [
-        "ts",
-        "js",
-        "json"
-      ],
-      setupFilesAfterEnv: ["<rootDir>/jest/matchers.ts", "jest-extended"],
-      testURL: "http://localhost"
+  displayName: 'unit',
+  transform: {
+    ".ts": "ts-jest"
+  },
+  testRegex: 'src/.*(\\.|/)(test|spec)\\.(ts|js)$',
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json"
+  ],
+  setupFilesAfterEnv: ["<rootDir>/jest/matchers.ts", "jest-extended"],
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  }
 };
