7eef86f94a65d39cc11caa539ec98ead33e52b73
madhur310 <madhur.shrivastava@salesforce.com>
2025-08-21 08:12:00 -0700

    fix: removing lodash as it won't be needed in DX

--- packages/lightning-lsp-common/jest/matchers.ts
+++ packages/lightning-lsp-common/jest/matchers.ts
@@ -1,13 +1,23 @@
 import { isAbsolute } from 'path';
 import * as fs from 'fs';
 
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace jest {
+        interface Matchers<R> {
+            toExist(): R;
+            toBeAbsolutePath(): R;
+            toEndWith(suffix: string): R;
+        }
+    }
+}
+
 expect.extend({
     toExist(path) {
         const pass = fs.existsSync(path);
         if (pass) {
             return {
-                message: () =>
-                    `expected ${path} not to exist`,
+                message: () => `expected ${path} not to exist`,
                 pass: true,
             };
         } else {
@@ -21,13 +31,26 @@ expect.extend({
         const pass = isAbsolute(path);
         if (pass) {
             return {
-                message: () =>
-                    `expected ${path} not to be absolute`,
+                message: () => `expected ${path} not to be absolute`,
                 pass: true,
             };
         } else {
             return {
                 message: () => `expected ${path} to be absolute`,
+                pass: false,
+            };
+        }
+    },
+    toEndWith(path: string, suffix: string) {
+        const pass = path.endsWith(suffix);
+        if (pass) {
+            return {
+                message: () => `expected ${path} not to end with ${suffix}`,
+                pass: true,
+            };
+        } else {
+            return {
+                message: () => `expected ${path} to end with ${suffix}`,
                 pass: false,
             };
         }
